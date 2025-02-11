7c450f3503b8b47adb092f311b63cd9727561cc4
RaVeN103 <privat@rv103.de>
2025-02-11 15:06:55 +0100

    1.2
    - Add Cloth Textures
    - GUI adapted
    - Buttons colored _worn, _damage, destruct, rotten

--- RwG_RVMat_Speedo.py
+++ RwG_RVMat_Speedo.py
@@ -49,11 +49,35 @@ def drop(event, texture, prefix, checkbox_values):
 
 def create_drop_area(root, texture, label_text, prefix, checkbox_values, row, column):
     """Erstellt ein individuelles Label mit Drop-Zone für jede Textur."""
-    frame = tk.Frame(root, padx=10, pady=10, relief=tk.RAISED, borderwidth=2)
-    frame.grid(row=row, column=column, padx=5, pady=5)
+    frame = tk.Frame(root, padx=5, pady=5, relief=tk.RAISED, borderwidth=2)
+    frame.grid(row=row, column=column, padx=5, pady=5, sticky="nsew")  # Stellt sicher, dass das Layout sauber bleibt
 
-    label = tk.Label(frame, text=f'Add {label_text}', padx=10, pady=10)
-    label.pack(fill=tk.X)
+    # Textfeld für die Beschriftung
+    text_widget = tk.Text(frame, height=1, width=19, borderwidth=0, bg=frame.cget("bg"))
+    text_widget.pack(fill=tk.BOTH, expand=True)
+    text_widget.insert(tk.END, label_text)
+    
+    # Textzentrierung
+    text_widget.tag_configure("center", justify="center")
+    text_widget.tag_add("center", "1.0", "end")
+
+    # Wörter farblich markieren
+    color_tags = {
+        "Worn": "gold",
+        "Damage": "orange",
+        "Destruct": "red",
+        "Burn": "brown",
+        "Rotten": "turquoise"
+        }
+
+    for word, color in color_tags.items():
+        if word in label_text:
+            start_idx = label_text.index(word)
+            end_idx = start_idx + len(word)
+            text_widget.tag_configure(color, foreground=color)
+            text_widget.tag_add(color, f"1.{start_idx}", f"1.{end_idx}")
+
+    text_widget.config(state=tk.DISABLED)  # Deaktivieren der Bearbeitung
 
     def on_enter(event):
         frame.config(bg='red')
@@ -66,16 +90,19 @@ def create_drop_area(root, texture, label_text, prefix, checkbox_values, row, co
         drop(event, texture, prefix, checkbox_values)
 
     root.drop_target_register(DND_FILES)
-    label.drop_target_register(DND_FILES)
-    label.dnd_bind('<<DropEnter>>', on_enter)
-    label.dnd_bind('<<DropLeave>>', on_leave)
-    label.dnd_bind('<<Drop>>', on_drop)
+    text_widget.drop_target_register(DND_FILES)
+    text_widget.dnd_bind('<<DropEnter>>', on_enter)
+    text_widget.dnd_bind('<<DropLeave>>', on_leave)
+    text_widget.dnd_bind('<<Drop>>', on_drop)
+
+
+
 
 def main():
     """Hauptprogramm für die Tkinter-GUI."""
     root = TkinterDnD.Tk()
-    root.title('RwG RVMat Speedo 1.1')
-    root.geometry('600x980')  # Breitere Fenstergröße für horizontales Layout
+    root.title('RwG RVMat Speedo 1.2')
+    root.geometry('740x800')  # Breitere Fenstergröße für horizontales Layout
 
     # Checkboxen für NOHQ, AS und SMDI (werden JETZT nach root erstellt)
     checkbox_values = {
@@ -108,50 +135,65 @@ def main():
     drop_frame_weapons = tk.Frame(root, relief=tk.RIDGE, borderwidth=3)
     drop_frame_weapons.grid(row=8, column=0, columnspan=3, padx=10, pady=10)
 
-    tk.Label(root, text="Weapons Metal Textures", font=("Arial", 14)).grid(row=9, column=0, columnspan=3)
-    drop_frame_weapons_metal = tk.Frame(root, relief=tk.RIDGE, borderwidth=3)
-    drop_frame_weapons_metal.grid(row=10, column=0, columnspan=3, padx=10, pady=10)
+    tk.Label(root, text="Cloth Textures", font=("Arial", 14)).grid(row=9, column=0, columnspan=3)
+    drop_frame_cloth = tk.Frame(root, relief=tk.RIDGE, borderwidth=3)
+    drop_frame_cloth.grid(row=10, column=0, columnspan=3, padx=10, pady=10)
 
-    tk.Label(root, text="Weapons Wood Textures", font=("Arial", 14)).grid(row=11, column=0, columnspan=3)
-    drop_frame_weapons_wood = tk.Frame(root, relief=tk.RIDGE, borderwidth=3)
-    drop_frame_weapons_wood.grid(row=12, column=0, columnspan=3, padx=10, pady=10)
-
-    tk.Label(root, text="Plastic Textures", font=("Arial", 14)).grid(row=13, column=0, columnspan=3)
+    tk.Label(root, text="Plastic Textures", font=("Arial", 14)).grid(row=11, column=0, columnspan=3)
     drop_frame_plastic = tk.Frame(root, relief=tk.RIDGE, borderwidth=3)
-    drop_frame_plastic.grid(row=14, column=0, columnspan=3, padx=10, pady=10)
+    drop_frame_plastic.grid(row=12, column=0, columnspan=3, padx=10, pady=10)
 
     # Texturen für Auswahl
     textures_generic = [
-        ('dz\\characters\\data\\generic_worn_mc.paa', 'Generic Worn', '_worn'),
-        ('dz\\characters\\data\\generic_damage_mc.paa', 'Generic Damage', '_damage'),
-        ('dz\\characters\\data\\generic_destruct_mc.paa', 'Generic Destruct', '_destruct')
+        ('dz\\characters\\data\\generic_worn_mc.paa', 'Worn', '_worn'),
+        ('dz\\characters\\data\\generic_damage_mc.paa', 'Damage', '_damage'),
+        ('dz\\characters\\data\\generic_destruct_mc.paa', 'Destruct', '_destruct')
     ]
 
     textures_wood =[
-        ('dz\\characters\\data\\generic_wood_worn_mc.paa', 'Wood Worn', '_worn'),
-        ('dz\\characters\\data\\generic_wood_damage_mc.paa', 'Wood Damage', '_damage'),
-        ('dz\\characters\\data\\generic_wood_destruct_mc.paa', 'Wood Destruct', '_destruct')
+        ('dz\\characters\\data\\generic_wood_worn_mc.paa', 'Worn', '_worn'),
+        ('dz\\characters\\data\\generic_wood_damage_mc.paa', 'Damage', '_damage'),
+        ('dz\\characters\\data\\generic_wood_destruct_mc.paa', 'Destruct', '_destruct')
     ]
     textures_food = [
-        ('dz\\gear\\food\\data\\food_generic_burn_mc.paa', 'Food Generic Burn', '_burnt'),
-        ('dz\\gear\\food\\data\\food_generic_rot_mc.paa', 'Food Generic Rotten', '_rotten'),
-        ('dz\\gear\\food\\data\\food_generic_rot2_mc.paa', 'Food Generic Rotten2', '_rotten')
+        ('dz\\gear\\food\\data\\food_generic_burn_mc.paa', 'Burn', '_burnt'),
+        ('dz\\gear\\food\\data\\food_generic_rot_mc.paa', 'Rotten', '_rotten'),
+        ('dz\\gear\\food\\data\\food_generic_rot2_mc.paa', 'Rotten2', '_rotten')
     ]
     textures_weapons = [
-        ('dz\\weapons\\data\\weapons_damage_generic_mc.paa', 'Weapons Damage Generic', '_damage'),
-        ('dz\\weapons\\data\\weapons_destruct_generic_mc.paa', 'Weapons Destruct Generic', '_destruct')
+        ('dz\\weapons\\data\\weapons_damage_generic_mc.paa', 'Damage Generic', '_damage'),
+        ('dz\\weapons\\data\\weapons_destruct_generic_mc.paa', 'Destruct Generic', '_destruct')
     ]
     textures_weapons_metal = [
-        ('dz\\weapons\\data\\weapons_damage_metal_mc.paa', 'Weapons Damage Metal', '_damage'),
-        ('dz\\weapons\\data\\weapons_destruct_metal_mc.paa', 'Weapons Destruct Metal', '_destruct')
+        ('dz\\weapons\\data\\weapons_damage_metal_mc.paa', 'Damage Metal', '_damage'),
+        ('dz\\weapons\\data\\weapons_destruct_metal_mc.paa', 'Destruct Metal', '_destruct')
     ]
     textures_weapons_wood = [
-        ('dz\\weapons\\data\\weapons_damage_wood_mc.paa', 'Weapons Damage Wood', '_damage'),
-        ('dz\\weapons\\data\\weapons_destruct_wood_mc.paa', 'Weapons Destrcut Wood', '_destruct')
+        ('dz\\weapons\\data\\weapons_damage_wood_mc.paa', 'Damage Wood', '_damage'),
+        ('dz\\weapons\\data\\weapons_destruct_wood_mc.paa', 'Destruct Wood', '_destruct')
     ]
     textures_plastic = [
-        ('dz\\characters\\data\\generic_plastic_damage_mc.paa', 'Plastic Damage', '_damage')
+        ('dz\\characters\\data\\generic_plastic_damage_mc.paa', 'Damage', '_damage')
     ]
+    textures_cloth_tops = [
+
+        ('dz\\characters\\tops\\data\\tops_damage_mc.paa', 'Tops Damage', '_damage'),
+        ('dz\\characters\\tops\\data\\tops_destruct_mc.paa', 'Tops Destruct', '_destruct')
+    ]
+    textures_cloth_vests = [    
+        ('dz\\characters\\vests\\data\\vests_damage_mc.paa', 'Vests Damage', '_damage'),
+        ('dz\\characters\\vests\\data\\vests_destruct_mc.paa', 'Vests Destruct', '_destruct')
+    ]
+    textures_cloth_pants = [
+        ('dz\\characters\\pants\\data\\pants_damage_mc.paa', 'Pants Damage', '_damage'),
+        ('dz\\characters\\pants\\data\\pants_destruct_mc.paa', 'Pants Destruct', '_destruct')
+    ]
+    textures_cloth_shoes = [    
+        ('dz\\characters\\shoes\\data\\shoes_damage_mc.paa', 'Shoes Damage', '_damage'),
+        ('dz\\characters\\shoes\\data\\shoes_destruct_mc.paa', 'Shoes Destruct', '_destruct')
+    ]
+        
+
 
     row = 0
     column = 0
@@ -177,17 +219,41 @@ def main():
         create_drop_area(drop_frame_weapons, texture, label_text, prefix, checkbox_values, row, column)
         column += 1
 
-    row = 0
+    row = 1
     column = 0
     for texture, label_text, prefix in textures_weapons_metal:
-        create_drop_area(drop_frame_weapons_metal, texture, label_text, prefix, checkbox_values, row, column)
+        create_drop_area(drop_frame_weapons, texture, label_text, prefix, checkbox_values, row, column)
+        column += 1
+
+    row = 2
+    column = 0
+    for texture, label_text, prefix in textures_weapons_wood:
+        create_drop_area(drop_frame_weapons, texture, label_text, prefix, checkbox_values, row, column)
         column += 1
 
     row = 0
     column = 0
-    for texture, label_text, prefix in textures_weapons_wood:
-        create_drop_area(drop_frame_weapons_wood, texture, label_text, prefix, checkbox_values, row, column)
-        column += 1
+    for texture, label_text, prefix in textures_cloth_tops:
+        create_drop_area(drop_frame_cloth, texture, label_text, prefix, checkbox_values, row, column)
+        row += 1
+    
+    row = 0
+    column = 1
+    for texture, label_text, prefix in textures_cloth_vests:
+        create_drop_area(drop_frame_cloth, texture, label_text, prefix, checkbox_values, row, column)
+        row += 1
+
+    row = 0
+    column = 2
+    for texture, label_text, prefix in textures_cloth_pants:
+        create_drop_area(drop_frame_cloth, texture, label_text, prefix, checkbox_values, row, column)
+        row += 1
+    
+    row = 0
+    column = 3
+    for texture, label_text, prefix in textures_cloth_shoes:
+        create_drop_area(drop_frame_cloth, texture, label_text, prefix, checkbox_values, row, column)
+        row += 1
 
     row = 0
     column = 0
